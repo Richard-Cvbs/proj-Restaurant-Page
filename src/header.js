9a606edb286ceb703f6c999076b563d0e584d765
Diego Felix <dt196397@gmail.com>
2022-02-25 11:41:12 -0600

    created header comp

--- src/header.js
+++ src/header.js
@@ -1,31 +1,33 @@
 import Restaurant_Icon from './Restaurant_Icon.svg';
+import sgvlobster from './img-sgv-lobster.svg';
+
 
 const headerComp = function (document){
-const createImg = function(){
+const createImg = function(reference, cssClass){
     const newImg = new Image();
-    newImg.className = Restaurant_Icon
-    newImg.src = Restaurant_Icon
+    newImg.className = cssClass
+    newImg.src = reference
     return newImg
 }
 const appendToHeader = function(Element){
     let header = document.querySelector('.header')
     header.appendChild(Element)
 }
-const createTextElement = function (TagNameString,Text){
+const createTextElement = function (TagNameString,cssClass,Text){
     let currentElement = document.createElement(TagNameString)
-    currentElement.className = TagNameString
+    currentElement.className = cssClass
     currentElement.textContent = Text
     return currentElement
 }
 const createHeader = function(){
-    const icon = createImg()
-    const Title = createTextElement("h1","Really Nice Place & Lobster")
-    const CornerText = createTextElement("div","Restaurant")
+    const icon = createImg(Restaurant_Icon,"header-icon")
+    const lobster = createImg(sgvlobster,"header-lobster")
+    const Title = createTextElement("h1","header-title","Really Nice Place & Lobster")
+    const CornerText = createTextElement("div","header-cornerText","Restaurant")
     appendToHeader(icon)
+    appendToHeader(lobster)
     appendToHeader(Title)
     appendToHeader(CornerText)
-
-
     }
     return{
         createHeader

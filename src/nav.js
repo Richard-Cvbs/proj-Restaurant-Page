71acc04f3241c1b27cd970e960ff77e3d27161ec
Diego Felix <dt196397@gmail.com>
2022-02-25 12:04:02 -0600

    + Nav-Comp

---
+++ src/nav.js
@@ -0,0 +1,26 @@
+const navComp = function (document){
+    const appendToNav = function(Element){
+        let nav = document.querySelector('.nav')
+        nav.appendChild(Element)
+    }
+    const createBttnElement = function (TagName,cssClass,Text){
+        let currentElement = document.createElement(TagName)
+        currentElement.className = cssClass
+        currentElement.textContent = Text
+        currentElement.setAttribute('type','button');
+        currentElement.setAttribute('value',Text);
+        return currentElement
+    }
+    const createNav = function(){
+            const aboutUs = createBttnElement("button","nav-button","About Us")
+            const menu = createBttnElement("button","nav-button","Menu")
+            const Contact = createBttnElement("button","nav-button","Contact")
+            appendToNav(aboutUs)
+            appendToNav(menu)
+            appendToNav(Contact)
+        }
+        return{
+            createNav
+        }
+    }(document)
+    export default navComp;

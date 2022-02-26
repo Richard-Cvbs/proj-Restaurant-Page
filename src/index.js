08d8e5ea80159b9c4c4da1d4fb1da10113741bbe
Diego Felix <dt196397@gmail.com>
2022-02-25 18:14:34 -0600

    +Change Content onclick + finished proj

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import mainComponent from './main_comp';
 import headerComp from './header';
 import navComp from './nav';
 import content1 from './content-1';
-import content2 from './content-2';
-import content3 from './content-3';
+import changeContent from './changeInBetween';
 
 
 import './style.css';
 
+//Create main structure
 mainComponent.createElementTree()
+//Create Static Header
 headerComp.createHeader()
+//Create Static Nav
 navComp.createNav()
-//content1.createContent()
-//content2.createContent()
-content3.createContent()
+//Create Inital Content
+content1.createContent()
+//Listen and Restructure Content As Necessary
+changeContent.bttnListen()
+
